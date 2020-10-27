7efc5c42a19eec545c406fb3e1f7227d864b3dda
Nour <ckastoun20@stuy.edu>
2020-10-27 17:07:45 -0400

    final version, commented out all test cases

---
+++ BankAccountTester.java
@@ -0,0 +1,31 @@
+public class BankAccountTester{
+  public static void main(String[] args) {
+/*  BankAccount acc1 = new BankAccount(12345, "hello");
+      System.out.println(acc1.toString()); //expecting 12345  0
+      acc1.setPassword("password");
+      System.out.println(acc1.getPassword()); //expecting password
+      System.out.println(acc1.deposit(24.32)); //expecting 24.32, 24.32, true
+      System.out.println(acc1.toString()); //expecting 12345 24.32
+      System.out.println(acc1.withdraw(23)); //expecting 23.0, 1.32, true
+*/
+
+/*  BankAccount acc2 = new BankAccount(2468, "hello");
+      acc2.setPassword("howdy");
+      System.out.println(acc2.getPassword()); //expecting howdy
+      System.out.println(acc2.deposit(50)); //expecting 50.0, 50.0, true
+      System.out.println(acc2.toString()); //expecting 2468 50.0
+      System.out.println(acc2.withdraw(-23)); //expecting -23.0, false
+      System.out.println(acc2.getBalance()); //expecting 50.0
+      System.out.println(acc2.withdraw(500)); //expecting 500.0, false
+      System.out.println(acc2.getBalance()); //expecting 50.0
+*/
+/*    BankAccount acc3 = new BankAccount(-341, "hello");
+      System.out.println(acc3.getPassword()); //expecting hello
+      acc3.setPassword("greetings, fellow human");
+      System.out.println(acc3.getPassword()); //expecting greetings, fellow human
+      System.out.println(acc3.deposit(-50)); //expecting -50.0, false
+      System.out.println(acc3.toString()); //expecting -341 0.0
+      System.out.println(acc3.getBalance()); //expecting 0.0
+*/
+  }
+}
