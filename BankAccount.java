3baae36a0fbcc5dcdac3dff789e924fabbfda6c3
Nour <ckastoun20@stuy.edu>
2020-10-27 15:58:34 -0400

    made constructor

---
+++ BankAccount.java
@@ -0,0 +1,22 @@
+public class BankAccount{
+
+  //Instance Variables
+  private double balance;
+  private int accountID;
+  private String password;
+
+  //1. constructor
+  public BankAccount(int a, String p){
+    balance = 0.0;
+    accountID = a;
+    password = p;
+  }
+
+  
+
+
+  public static void main(String[] args) {
+//tests are in Tester.java
+  }
+
+}
